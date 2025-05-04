d9432d3487bade9f18f7a00d0a9225533f5a9128
Miroslav Balev <zevs8@abv.bg>
2025-05-04 16:32:27 +0300

    Add String class

---
+++ String/String.cpp
@@ -0,0 +1,279 @@
+#include "String.h"
+
+static unsigned roundToPowerOfTwo(unsigned v) {
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    return v;
+}
+
+void String::freeDynamic() {
+    delete[] elements;
+}
+
+void String::copyFromDynamic(const String& other) {
+    size = other.size;
+    capacity = other.capacity;
+
+    elements = new char[capacity + 1];
+    std::strcpy(elements, other.elements);
+}
+
+void String::resize(int newCapacity) {
+    capacity = newCapacity;
+
+    char* newElements = new char[capacity + 1];
+    std::strcpy(newElements, elements);
+
+    delete[] elements;
+    elements = newElements;
+}
+
+String::String() {
+    size = 0;
+    capacity = 0;
+    elements = new char[capacity];
+    elements[0] = '\0';
+}
+
+String::String(int capacity) {
+    size = 0;
+    this->capacity = capacity;
+    elements = new char[capacity];
+    elements[0] = '\0';
+}
+
+String::String(size_t size, const char* initialValue) {
+    if (!initialValue) {
+        return;
+    }
+
+    this->size = std::strlen(initialValue);
+    capacity = roundToPowerOfTwo(size);
+    elements = new char[capacity + 1];
+    std::strcpy(elements, initialValue);
+}
+
+String::String(const String& other) {
+    copyFromDynamic(other);
+}
+
+String::String(size_t size, char c) {
+    this->size = size;
+    capacity = roundToPowerOfTwo(size);
+    elements = new char[capacity + 1];
+
+    for (int i = 0; i < size; i++) {
+        elements[i] = c;
+    }
+    elements[size] = '\0';
+}
+
+String::~String() {
+    freeDynamic();
+}
+
+String& String::operator=(const String& other) {
+    if (this != &other) {
+        freeDynamic();
+        copyFromDynamic(other);
+    }
+
+    return *this;
+}
+
+size_t String::getSize() const {
+    return size;
+}
+
+int String::getCapacity() const {
+    return capacity;
+}
+
+const char* String::getElements() const {
+    return elements;
+}
+
+void String::popBack() {
+    if (size == 0) {
+        throw std::out_of_range("The array is empty");
+    }
+
+    size--;
+    elements[size] = '\0';
+}
+
+void String::pushBack(char newElement) {
+    if (size + 1 >= capacity) {
+        resize(capacity * 2);
+    }
+
+    elements[size++] = newElement;
+    elements[size] = '\0';
+}
+
+char& String::at(int index) {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+
+    return elements[index];
+}
+
+const char& String::at(int index) const {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+
+    return elements[index];
+}
+
+void String::clear() {
+    size = 0;
+    elements[0] = '\0';
+}
+
+bool String::empty() {
+    return size == 0;
+}
+
+void String::shrinkToFit() {
+    if (size == capacity) {
+        return;
+    }
+
+    char* newElements = new char[size + 1];
+    std::strcpy(newElements, elements);
+
+    delete[] elements;
+    elements = newElements;
+    capacity = size;
+}
+
+char& String::operator[](int index) {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+
+    return elements[index];
+}
+
+const char& String::operator[](int index) const {
+    if (index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+
+    return elements[index];
+}
+
+String& String::operator+=(const String& str) {
+    size_t newLength = size + str.size;
+    if (newLength >= capacity) {
+        resize(newLength * 2);
+    }
+
+    std::strcat(elements, str.elements);
+    size += str.size;
+
+    return *this;
+}
+
+String& String::operator+=(const char* str) {
+    size_t addLength = std::strlen(str);
+    size_t newLength = size + addLength;
+    if (newLength >= capacity) {
+        resize(newLength * 2);
+    }
+
+    std::strcat(elements, str);
+    size += addLength;
+
+    return *this;
+}
+
+String& String::operator+=(char c) {
+    pushBack(c);
+    return *this;
+}
+
+String operator+(const String& lhs, const String& rhs) {
+    String lhsCopy(lhs);
+
+    lhsCopy += rhs;
+    return lhsCopy;
+}
+
+String operator+(const String& lhs, const char* rhs) {
+    String lhsCopy(lhs);
+
+    lhsCopy += rhs;
+    return lhsCopy;
+}
+
+String operator+(const char* lhs, const String& rhs) {
+    String str(std::strlen(lhs), lhs);
+
+    str += rhs;
+    return str;
+}
+
+String operator+(const String& lhs, char rhs) {
+    String lhsCopy(lhs);
+
+    lhsCopy += rhs;
+    return lhsCopy;
+}
+
+String operator+(char lhs, const String& rhs) {
+    String str(1, lhs);
+
+    str += rhs;
+    return str;
+}
+
+bool operator==(const String& lhs, const String& rhs) {
+    return std::strcmp(lhs.elements, rhs.elements) == 0;
+}
+
+bool operator!=(const String& lhs, const String& rhs) {
+    return !(lhs == rhs);
+}
+
+bool operator<(const String& lhs, const String& rhs) {
+    return std::strcmp(lhs.elements, rhs.elements) < 0;
+}
+
+bool operator>(const String& lhs, const String& rhs) {
+    return std::strcmp(lhs.elements, rhs.elements) > 0;
+}
+
+bool operator<=(const String& lhs, const String& rhs) {
+    return !(lhs > rhs);
+}
+
+bool operator>=(const String& lhs, const String& rhs) {
+    return !(lhs < rhs);
+}
+
+std::ostream& operator<<(std::ostream& os, const String& str) {
+    os << str.getElements();
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, String& str) {
+    char buffer[1024];
+    is >> buffer;
+
+    size_t inputLen = std::strlen(buffer);
+    int newCap = roundToPowerOfTwo(inputLen);
+    str.resize(newCap);
+
+    std::strcpy(str.elements, buffer);
+    str.size = inputLen;
+
+    return is;
+}
---
+++ String/String.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <iostream>
+
+static unsigned roundToPowerOfTwo(unsigned v);
+
+class String {
+    char* elements;
+    int capacity;
+    size_t size;
+
+    void resize(int newCapacity);
+
+    void copyFromDynamic(const String& other);
+
+    void freeDynamic();
+
+    public:
+      String();
+
+      String(int capacity);
+
+      String(size_t size, const char* initialValue);
+
+      String(const String& other);
+
+      String(size_t size, char c);
+
+      ~String();
+
+      String& operator=(const String& other);
+
+      size_t getSize() const;
+
+      int getCapacity() const;
+
+      const char* getElements() const;
+
+      void popBack();
+
+      void pushBack(char newElement);
+
+      char& at(int index);
+
+      const char& at(int index) const;
+
+      void clear();
+
+      bool empty();
+
+      void shrinkToFit();
+
+      char& operator[](int index);
+
+      const char& operator[](int index) const;
+
+      friend std::istream& operator>>(std::istream& is, String& str);
+
+      String& operator+=(const String& str);
+
+      String& operator+=(const char* str);
+
+      String& operator+=(char c);
+
+      friend bool operator==(const String& lhs, const String& rhs);
+
+      friend bool operator!=(const String& lhs, const String& rhs);
+
+      friend bool operator<(const String& lhs, const String& rhs);
+
+      friend bool operator>(const String& lhs, const String& rhs);
+
+      friend bool operator<=(const String& lhs, const String& rhs);
+
+      friend bool operator>=(const String& lhs, const String& rhs);
+};
+
+bool operator==(const String& lhs, const String& rhs);
+bool operator!=(const String& lhs, const String& rhs);
+bool operator<(const String& lhs, const String& rhs);
+bool operator>(const String& lhs, const String& rhs);
+bool operator<=(const String& lhs, const String& rhs);
+bool operator>=(const String& lhs, const String& rhs);
+
+String operator+(const String& lhs, const String& rhs);
+String operator+(const String& lhs, const char* rhs);
+String operator+(const char* lhs, const String& rhs);
+String operator+(const String& lhs, char rhs);
+String operator+(char lhs, const String& rhs);
+
+std::ostream& operator<<(std::ostream& os, const String& str);
+std::istream& operator>>(std::istream& is, String& str);
